9dcc7ac1f36c960b43462b757e9bee703b7edb55
Flawid D'Souza <me@flawiddsouza.com>
2018-08-24 14:13:39 +0530

    Done

---
+++ checkbox-multiselect.js
@@ -0,0 +1,42 @@
+Vue.component('multiselect', {
+    props: [
+        'items', 'text', 'value', 'selectedItems'
+    ],
+    watch: {
+        selectedItems() {
+            this.selectedItemsVirtual = this.selectedItems
+        }
+    },
+    data: function () {
+        return {
+            rootStyle: {
+                display: 'inline-block',
+                padding: '.429rem .929rem',
+                border: '1px solid #e0e0e0',
+                'border-radius': '.215rem',
+                height: '10em',
+                'min-width': '10em',
+                color: '#757575',
+                'overflow-y': 'auto',
+                'font-family': 'sans-serif'
+            },
+            selectedItemsVirtual: []
+        }
+    },
+    template: `
+        <div :style="rootStyle">
+            <div v-for="item in items">
+                <label><input type="checkbox" :value="item[value]" @change="handleChange($event)" v-model="selectedItemsVirtual"> {{ item[text] }}</label>
+            </div>
+        </div>
+    `,
+    methods: {
+        handleChange(e) {
+            // console.log(this.selectedItemsVirtual)
+            this.$emit('update:selected-items', this.selectedItemsVirtual)
+        }
+    },
+    created() {
+        this.selectedItemsVirtual = this.selectedItems
+    }
+})
